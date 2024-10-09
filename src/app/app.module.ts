59d61fe3dbeb38e474b65ebd48d199cd6650b50d
thiagonunes <thiago_nunes11@hotmail.com>
2024-10-09 09:20:03 -0300

    Iniciando o repositório

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './shared/material/material.module';
+import { ClienteModule } from "./pages/cliente/cliente.module";
+import { provideNgxMask } from 'ngx-mask';
+import { CadastroContaComponent } from './pages/conta/cadastro-conta/cadastro-conta.component';
+import { ListagemContaComponent } from './pages/conta/listagem-conta/listagem-conta.component';
+import { DepositoContaComponent } from './pages/conta/deposito-conta/deposito-conta.component';
+import { SaqueContaComponent } from './pages/conta/saque-conta/saque-conta.component';
+import { TransferenciaContaComponent } from './pages/conta/transferencia-conta/transferencia-conta.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroContaComponent,
+    ListagemContaComponent,
+    DepositoContaComponent,
+    SaqueContaComponent,
+    TransferenciaContaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialModule,
+    ClienteModule
+],
+  providers: [provideNgxMask()],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/cliente/cliente.module.ts
@@ -0,0 +1,36 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { ListagemClienteComponent } from './listagem-cliente/listagem-cliente.component';
+
+
+@NgModule({
+  declarations: [
+    CadastroClienteComponent,
+    ListagemClienteComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HttpClientModule,
+    MaterialModule,
+    NgxMaskDirective,
+    NgxMaskPipe
+   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [provideNgxMask()],
+  exports: [
+    CadastroClienteComponent,
+    CadastroClienteComponent
+  ]
+})
+export class ClienteModule { }
